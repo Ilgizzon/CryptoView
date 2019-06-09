db5345d32655c85b5bcfe8a8f802c7ffb81b1963
ilgiz <fazliev.ilgiz@yandex.ru>
2019-06-09 13:34:04 +0500

    init commit

---
+++ CryptoView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CryptoView
+//
+//  Created by Ilgiz Fazlyev on 09/06/2019.
+//  Copyright © 2019 Ilgiz Fazlyev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
