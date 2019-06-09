31f7baf248cee014e7eab3c1247d6edf0ec309dc
ilgiz <fazliev.ilgiz@yandex.ru>
2019-06-09 23:27:28 +0500

    added network service

---
+++ CryptoView/Services/Network/Network.swift
@@ -0,0 +1,27 @@
+//
+//  Network.swift
+//  CryptoView
+//
+//  Created by Ilgiz Fazlyev on 09/06/2019.
+//  Copyright © 2019 Ilgiz Fazlyev. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Network {
+   
+    static func getCryptoList(completion: @escaping (String) -> Void?) {
+        
+        AF.request(Constants.URL, method: .get).responseDecodable{(response: DataResponse<CryptoList>)  in
+            
+            switch response.result {
+            case .success(let data):
+                DB.updateOrAddData(data: data)
+            case .failure(let error):
+                completion(error.localizedDescription)
+            }
+        }
+        
+    }
+}
